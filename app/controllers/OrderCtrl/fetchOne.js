0a9aa745ba52759094cfd040c079845e9b8e55f5
Remah Amr <remah.alarabi@hamza.solutions>
2020-10-20 21:23:08 +0200

    add fetch One order

---
+++ app/controllers/OrderCtrl/fetchOne.js
@@ -0,0 +1,10 @@
+const $baseCtrl = require("../$baseCtrl");
+const models = require("../../models");
+const APIResponse = require("../../utils/APIResponse");
+
+module.exports = $baseCtrl(async (req, res) => {
+    const order = await models.order.findById(parseInt(req.params.id))
+    if(!order) return APIResponse.NotFound(res,'No order with that id ')
+
+    return APIResponse.Ok(res,order)
+});
--- app/routes/resources/order.route.js
+++ app/routes/resources/order.route.js
@@ -6,5 +6,6 @@ let router = express.Router();
 router.post('/order',policies.isAllowed('customer'),ctrls.OrderCtrl.createOrder)
 
 router.get('/orders',policies.isAllowed(['admin','secretary']),ctrls.OrderCtrl.fetchAll)
+router.get('/orders/:id',policies.isAllowed(['admin','secretary']),ctrls.OrderCtrl.fetchOne)
 
 module.exports = router;
