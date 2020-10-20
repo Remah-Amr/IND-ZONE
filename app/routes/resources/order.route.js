30434d183d45f3542454a21f77519a6af37a0bb8
Remah Amr <remah.alarabi@hamza.solutions>
2020-10-20 22:10:12 +0200

    end point namej

--- app/routes/resources/order.route.js
+++ app/routes/resources/order.route.js
@@ -3,7 +3,7 @@ const ctrls = require("../../controllers");
 const policies = require('../../policies')
 let router = express.Router();
 
-router.post('/order',policies.isAllowed('customer'),ctrls.OrderCtrl.createOrder)
+router.post('/orders',policies.isAllowed('customer'),ctrls.OrderCtrl.createOrder)
 
 router.get('/orders',policies.isAllowed(['admin','secretary']),ctrls.OrderCtrl.fetchAll)
 router.get('/orders/:id',policies.isAllowed(['admin','secretary']),ctrls.OrderCtrl.fetchOne)
